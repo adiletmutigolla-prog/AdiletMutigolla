2a11c249e97ba45763761342a782c3903bacd830
Admin <adilet.mutigolla@narxoz.kz>
2026-05-08 16:18:22 +0500

    endterm

--- CourseManager.py
+++ CourseManager.py
@@ -6,7 +6,7 @@ class CourseManager:
         self.courses = pd.read_csv(courses_path)
 
     def process_data(self):
-        self.courses = self.courses.drop_duplicates(subset=['course_code']).sort_values('course_code')
+        self.courses = self.courses.drop_duplicates(subset=['course_code'])
 
         merged_df = pd.merge(self.enroll, self.courses, on='course_code')
         return merged_df.groupby('title')['student_id'].count().reset_index()
